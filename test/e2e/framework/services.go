6893f88bb68347eb6e69bdc785357442732165d8
Tom Pantelis <tompantelis@gmail.com>
2020-03-14 13:55:41 -0400

    Add reusable Framework features from lighthouse
    - Framework.ExecWithOptions function
    - Service-related functions
    - Deployment-related functions
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- test/e2e/framework/services.go
+++ test/e2e/framework/services.go
@@ -3,6 +3,8 @@ package framework
 import (
 	"fmt"
 
+	"github.com/onsi/ginkgo"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -14,7 +16,6 @@ const (
 )
 
 func (f *Framework) CreateTCPService(cluster ClusterIndex, selectorName string, port int) *v1.Service {
-
 	tcpService := v1.Service{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: fmt.Sprintf("test-svc-%s", selectorName),
@@ -46,4 +47,46 @@ func (f *Framework) CreateTCPService(cluster ClusterIndex, selectorName string, 
 
 		return service, err
 	}, NoopCheckResult).(*v1.Service)
+}
+
+func (f *Framework) NewNginxService(cluster ClusterIndex) *corev1.Service {
+	var port int32 = 80
+	nginxService := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "nginx-demo",
+			Labels: map[string]string{
+				"app": "nginx-demo",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type: "ClusterIP",
+			Ports: []corev1.ServicePort{
+				{
+					Port:     port,
+					Protocol: corev1.ProtocolTCP,
+					TargetPort: intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: 80,
+					},
+				},
+			},
+			Selector: map[string]string{
+				"app": "nginx-demo",
+			},
+		},
+	}
+
+	sc := f.ClusterClients[cluster].CoreV1().Services(f.Namespace)
+	service := AwaitUntil("create service", func() (interface{}, error) {
+		return sc.Create(&nginxService)
+
+	}, NoopCheckResult).(*v1.Service)
+	return service
+}
+
+func (f *Framework) DeleteService(cluster ClusterIndex, serviceName string) {
+	ginkgo.By(fmt.Sprintf("Deleting service %q on %q", serviceName, TestContext.ClusterIDs[cluster]))
+	AwaitUntil("delete service", func() (interface{}, error) {
+		return nil, f.ClusterClients[cluster].CoreV1().Services(f.Namespace).Delete(serviceName, &metav1.DeleteOptions{})
+	}, NoopCheckResult)
 }
