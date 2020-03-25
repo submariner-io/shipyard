dd84449212c751546fd365f795cee7f54aae036f
Stephen Kitt <skitt@redhat.com>
2020-03-25 15:09:40 +0100

    Validate the tests we ship (#51)
    Since the point of shipyard is to provide a test framework, we should
    validate that framework. Doing so reveals a dependency on submariner
    which this patch also removes.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	_ "github.com/submariner-io/shipyard/test/e2e/example"
-	_ "github.com/submariner-io/submariner/test/e2e/framework"
+	_ "github.com/submariner-io/shipyard/test/e2e/framework"
 )
 
 func TestE2E(t *testing.T) {
