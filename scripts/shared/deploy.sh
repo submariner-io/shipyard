9d51eed0e4013df6a7c489f41fc0d64c1032099c
Mike Kolesnik <mkolesni@redhat.com>
2020-04-30 12:01:08 +0300

    Use boolean globalnet flag in deploy.sh also
    Forgot to change it here, use a boolean flag.

--- scripts/shared/deploy.sh
+++ scripts/shared/deploy.sh
@@ -7,13 +7,13 @@ DEFINE_string 'cluster_settings' '' "Settings file to customize cluster deployme
 DEFINE_string 'deploytool' 'operator' 'Tool to use for deploying (operator/helm)'
 DEFINE_string 'deploytool_broker_args' '' 'Any extra arguments to pass to the deploytool when deploying the broker'
 DEFINE_string 'deploytool_submariner_args' '' 'Any extra arguments to pass to the deploytool when deploying submariner'
-DEFINE_string 'globalnet' 'false' "Deploy with operlapping CIDRs (set to 'true' to enable)"
+DEFINE_boolean 'globalnet' false "Deploy with operlapping CIDRs (set to 'true' to enable)"
 DEFINE_string 'cable_driver' '' "Cable driver implementation"
 
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
 
-globalnet="${FLAGS_globalnet}"
+[[ "${FLAGS_globalnet}" = "${FLAGS_TRUE}" ]] && globalnet=true || globalnet=false
 deploytool="${FLAGS_deploytool}"
 deploytool_broker_args="${FLAGS_deploytool_broker_args}"
 deploytool_submariner_args="${FLAGS_deploytool_submariner_args}"
