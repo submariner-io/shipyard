33bbd9975e59b4d389258ae4461be014fd6d0f61
Mike Kolesnik <mkolesni@redhat.com>
2020-05-13 15:19:24 +0300

    Fix post_mortem script and add a test for it
    Fixed the script to work after PR #113 changed the way run_ functions
    work.
    
    Also added a simple test to verify that the post_mortem itself is
    working.

---
+++ test/scripts/post_mortem/test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ${SCRIPTS_DIR}/lib/cluster_settings
+source ${SCRIPTS_DIR}/lib/debug_functions
+source ${SCRIPTS_DIR}/lib/utils
+
+function image_name() {
+    echo "norepo/${cluster}_image"
+}
+
+function deploy_deadshoot() {
+    filename="/tmp/netshoot_${cluster}"
+    cp ${RESOURCES_DIR}/netshoot.yaml "${filename}"
+
+    sed -i "s#image: .*#image: $(image_name)#" "${filename}"
+
+    kubectl apply -f "${filename}"
+}
+
+declare_kubeconfig
+run_all_clusters deploy_deadshoot
+
+post_mortem=$(make post-mortem)
+echo "$post_mortem"
+
+for cluster in "${clusters[@]}"; do
+    img=$(image_name)
+    if [[ ! $post_mortem =~ $(image_name) ]]; then
+        echo "Post mortem failed, didn't find failure for ${cluster}"
+        exit 1
+    fi
+done
