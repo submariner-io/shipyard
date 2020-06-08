3e1ed401cb7b44408667889957b7427ab5bde7c4
Mike Kolesnik <mkolesni@redhat.com>
2020-06-08 13:26:52 +0300

    Add shared unit test script
    Adding a shared script (and accompanying Makefile target).
    
    To make it more robust (and flexible), the find_go_pkg_dirs function was
    rewritten (and simplified) in the process.

---
+++ scripts/shared/unit_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ${SCRIPTS_DIR}/lib/debug_functions
+source ${SCRIPTS_DIR}/lib/find_functions
+
+echo "Looking for packages to test"
+
+# This can’t be done as simply with parameter substitution
+# shellcheck disable=SC2001
+packages=". $(find_go_pkg_dirs "$@" | sed -e 's![^ ]*!./&/...!g')"
+
+echo "Running tests in ${packages}"
+[ "${ARCH}" == "amd64" ] && race=-race
+go test -v ${race} -cover ${packages} -ginkgo.reportPassed -ginkgo.reportFile junit.xml
