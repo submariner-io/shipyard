9a374650ddd45ba04638981f08853450fa1e240a
Stephen Kitt <steve@sk2.org>
2021-01-15 12:52:52 +0100

    Revert "Don't tag as `devel` by default"
    This reverts commit 0f86a164eef4b98c8d55457e19ce78e62656e065.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/shared/release.sh
+++ scripts/shared/release.sh
@@ -4,7 +4,7 @@
 
 source ${SCRIPTS_DIR}/lib/shflags
 source ${SCRIPTS_DIR}/lib/version
-DEFINE_string 'tag' '' "Additional tag(s) to use for the image (prefix 'v' will be stripped)"
+DEFINE_string 'tag' "${CUTTING_EDGE}" "Additional tag(s) to use for the image (prefix 'v' will be stripped)"
 DEFINE_string 'repo' 'quay.io/submariner' "Quay.io repo to deploy to"
 FLAGS_HELP="USAGE: $0 [--tag v1.2.3] [--repo quay.io/myrepo] image [image ...]"
 FLAGS "$@" || exit $?
