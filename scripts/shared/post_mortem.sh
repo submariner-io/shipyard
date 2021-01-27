465ee004aaa9ca4e40e858c731899fb1caa98fb4
Mike Kolesnik <mkolesni@redhat.com>
2021-01-27 13:59:58 +0200

    Extract post-mortem composite GHA
    Extracting the post-mortem to a composite GHA that can be used by other
    projects.
    As part of this change, GitHub specific prints have been added to make
    the GHA log easier to consume.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/shared/post_mortem.sh
+++ scripts/shared/post_mortem.sh
@@ -5,19 +5,26 @@ source ${SCRIPTS_DIR}/lib/utils
 
 ### Functions ###
 
+function print_section() {
+    echo "===================================================================="
+    echo "::endgroup::"
+    echo "::group::$*"
+    echo "======================= $* ======================="
+}
+
 function post_analyze() {
-    echo "======================= Post mortem $cluster ======================="
+    print_section "** Pods not running in $cluster **"
     kubectl get all --all-namespaces
     for pod in $(kubectl get pods -A | tail -n +2 | grep -v Running | sed 's/  */;/g'); do
         ns=$(echo $pod | cut -f1 -d';')
         name=$(echo $pod | cut -f2 -d';')
-        echo "======================= $name - $ns ============================"
+        print_section "NS: $ns; Pod: $name"
         kubectl -n $ns describe pod $name
         kubectl -n $ns logs $name
-        echo "===================== END $name - $ns =========================="
     done
 
     # TODO (revisit): The following is added to debug intermittent globalnet failures.
+    print_section "** Globalnet related logs in $cluster **"
     namespace="kube-system"
     for pod in $(kubectl get pods --selector=k8s-app=kube-proxy -n $namespace -o jsonpath='{.items[*].metadata.name}'); do
         echo "+++++++++++++++++++++: Logs for Pod $pod in namespace $namespace :++++++++++++++++++++++"
@@ -70,12 +77,12 @@ function post_analyze() {
 
     echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     subctl show all
-
-    echo "===================== END Post mortem $cluster ====================="
+    return 0
 }
 
 ### Main ###
 
 declare_kubeconfig
-clusters=($(kind get clusters))
-run_sequential "${clusters[*]}" post_analyze
+for cluster in $(kind get clusters); do
+    post_analyze
+done
