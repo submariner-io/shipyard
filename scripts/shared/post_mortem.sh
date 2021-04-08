c0997b93c44eb546f3ef5e596d205c51e8a3d3d4
Miguel Angel Ajo <majopela@redhat.com>
2021-04-08 17:27:13 +0200

    Add kube-controller-manager logs to postmortem
    Recent issues with subctl & the operator could only be detected via
    logs in the kube-controller-manager (due to API update issues which
    didn't propagate to the API requests).
    
    This commit adds the kube-controller-manager logs to postmortem
    output.
    
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- scripts/shared/post_mortem.sh
+++ scripts/shared/post_mortem.sh
@@ -47,6 +47,9 @@ function post_analyze() {
     print_section "* Kube-proxy pod logs for $cluster *"
     print_pods_logs "kube-system" "k8s-app=kube-proxy"
 
+    print_section "* Kube-controller-manager pod logs for $cluster *"
+    print_pods_logs "kube-system" "component=kube-controller-manager"
+
     print_section "* Submariner-operator pod logs for $cluster *"
     print_pods_logs "submariner-operator"
 
