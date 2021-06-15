39270f7fb0cf00c17de3d26aac6dfd63784cbbf9
Sridhar Gaddam <sgaddam@redhat.com>
2021-06-15 20:45:28 +0530

    Add APIs to read GlobalIP for GN2.0 objects
    This PR implements APIs that return the GlobalIPs part of ClusterGlobalEgressIP
    as well as GlobalIngressIP objects.
    
    Signed-Off-by: Sridhar Gaddam <sgaddam@redhat.com>
    Co-authored-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

---
+++ test/e2e/framework/clusterglobalegressip.go
@@ -0,0 +1,76 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package framework
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+)
+
+var clusterGlobalEgressIPGVR = &schema.GroupVersionResource{
+	Group:    "submariner.io",
+	Version:  "v1",
+	Resource: "clusterglobalegressips",
+}
+
+func (f *Framework) AwaitClusterGlobalEgressIPs(cluster ClusterIndex, name string) []string {
+	if TestContext.GlobalnetEnabled {
+		gipClient := clusterGlobalEgressIPClient(cluster)
+		obj := AwaitUntil(fmt.Sprintf("await ClusterGlobalEgressIP %s", name),
+			func() (interface{}, error) {
+				resGip, err := gipClient.Get(context.TODO(), name, metav1.GetOptions{})
+				if apierrors.IsNotFound(err) {
+					return nil, nil
+				}
+				return resGip, err
+			},
+			func(result interface{}) (bool, string, error) {
+				if result == nil {
+					return false, fmt.Sprintf("ClusterGlobalEgressIP %s not found yet", name), nil
+				}
+
+				globalIPs := getGlobalIPs(result.(*unstructured.Unstructured))
+				if len(globalIPs) == 0 {
+					return false, fmt.Sprintf("ClusterGlobalEgressIP %q exists but allocatedIPs not available yet", name), nil
+				}
+				return true, "", nil
+			})
+
+		return getGlobalIPs(obj.(*unstructured.Unstructured))
+	}
+	return []string{}
+}
+
+func clusterGlobalEgressIPClient(cluster ClusterIndex) dynamic.ResourceInterface {
+	return DynClients[cluster].Resource(*clusterGlobalEgressIPGVR).Namespace(corev1.NamespaceAll)
+}
+
+func getGlobalIPs(obj *unstructured.Unstructured) []string {
+	if obj != nil {
+		globalIPs, _, _ := unstructured.NestedStringSlice(obj.Object, "status", "allocatedIPs")
+		return globalIPs
+	}
+	return []string{}
+}
