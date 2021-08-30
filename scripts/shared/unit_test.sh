04485134b02f1c821800374c03b81786d9095ca7
Stephen Kitt <skitt@redhat.com>
2021-08-30 16:58:20 +0200

    Run unit tests in multiple modules
    In repositories with multiple modules, tests must be run in each
    module separately, excluding any packages which are in a
    “sub-module”.
    
    This fixes unit_test.sh to look for modules, and run the tests in each
    module. If tests fail, subsequent modules will still be tested, but
    the overall result will reflect the failure.
    
    Fixes: #626
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/shared/unit_test.sh
+++ scripts/shared/unit_test.sh
@@ -7,8 +7,27 @@ source ${SCRIPTS_DIR}/lib/find_functions
 
 echo "Looking for packages to test"
 
-packages="$(find_unit_test_dirs "$@")"
+modules=($(find_modules))
 
-echo "Running tests in ${packages}"
-[ "${ARCH}" == "amd64" ] && race=-race
-${GO:-go} test -v ${race} -cover ${packages} -ginkgo.v -ginkgo.trace -ginkgo.reportPassed -ginkgo.reportFile junit.xml
+result=0
+
+for module in "${modules[@]}"; do
+    printf "Looking for tests in module %s\n" ${module}
+
+    excluded_modules=""
+    for exc_module in "${modules[@]}"; do
+	if [ "$exc_module" != "$module" -a "$exc_module" != "." ]; then
+	    excluded_modules+=" ${exc_module:2}"
+	fi
+    done
+
+    packages="$(cd $module; find_unit_test_dirs "$excluded_modules" "$@" | tr '\n' ' ')"
+
+    if [ -n "${packages}" ]; then
+	echo "Running tests in ${packages}"
+	[ "${ARCH}" == "amd64" ] && race=-race
+	(cd $module && ${GO:-go} test -v ${race} -cover ${packages} -ginkgo.v -ginkgo.trace -ginkgo.reportPassed -ginkgo.reportFile junit.xml) || result=1
+    fi
+done
+
+exit $result
