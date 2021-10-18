e85fb1e99f7a4ae73f36cf10187cc2a67674257b
Stephen Kitt <skitt@redhat.com>
2021-10-18 11:24:30 +0200

    Use shas for kind node images
    This approach is recommended by upstream.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/shared/clusters.sh
+++ scripts/shared/clusters.sh
@@ -4,12 +4,12 @@
 # See the release notes of the kind version in use
 DEFAULT_K8S_VERSION=1.20
 declare -A kind_k8s_versions
-kind_k8s_versions[1.17]=1.17.17
-kind_k8s_versions[1.18]=1.18.19
-kind_k8s_versions[1.19]=1.19.11
-kind_k8s_versions[1.20]=1.20.7
-kind_k8s_versions[1.21]=1.21.1
-kind_k8s_versions[1.22]=1.22.0
+kind_k8s_versions[1.17]=1.17.17@sha256:66f1d0d91a88b8a001811e2f1054af60eef3b669a9a74f9b6db871f2f1eeed00
+kind_k8s_versions[1.18]=1.18.19@sha256:7af1492e19b3192a79f606e43c35fb741e520d195f96399284515f077b3b622c
+kind_k8s_versions[1.19]=1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
+kind_k8s_versions[1.20]=1.20.7@sha256:cbeaf907fc78ac97ce7b625e4bf0de16e3ea725daf6b04f930bd14c67c671ff9
+kind_k8s_versions[1.21]=1.21.1@sha256:69860bda5563ac81e3c0057d654b5253219618a22ec3a346306239bba8cfa1a6
+kind_k8s_versions[1.22]=1.22.0@sha256:b8bda84bb3a190e6e028b1760d277454a72267a5454b57db34437c34a588d047
 
 ## Process command line flags ##
 
