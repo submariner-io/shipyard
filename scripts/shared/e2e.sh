9c6a337ddb1b82edb7ab5a7fdbe690aeb87206a5
Mike Kolesnik <mkolesni@redhat.com>
2021-11-09 14:33:44 +0200

    Remove settings flag from E2E arguments
    The E2E scriopt is not using anything from the settings, so there's no
    need to load them.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/shared/e2e.sh
+++ scripts/shared/e2e.sh
@@ -3,8 +3,6 @@
 ## Process command line flags ##
 
 source ${SCRIPTS_DIR}/lib/shflags
-DEFINE_string 'cluster_settings' '' "Settings file to customize cluster deployments (deprecated, use settings instead)"
-DEFINE_string 'settings' '' "Settings YAML file to customize cluster deployments"
 DEFINE_string 'focus' '' "Ginkgo focus for the E2E tests"
 DEFINE_string 'skip' '' "Ginkgo skip for the E2E tests"
 DEFINE_string 'testdir' 'test/e2e' "Directory under to be used for E2E testing"
@@ -18,8 +16,6 @@ eval set -- "${FLAGS_ARGV}"
 ginkgo_args=()
 [[ -n "${FLAGS_focus}" ]] && ginkgo_args+=("-ginkgo.focus=${FLAGS_focus}")
 [[ -n "${FLAGS_skip}" ]] && ginkgo_args+=("-ginkgo.skip=${FLAGS_skip}")
-cluster_settings="${FLAGS_cluster_settings}"
-settings="${FLAGS_settings}"
 [[ "${FLAGS_lazy_deploy}" = "${FLAGS_TRUE}" ]] && lazy_deploy=true || lazy_deploy=false
 
 if [[ $# == 0 ]]; then
@@ -81,7 +77,6 @@ function test_with_subctl {
 
 ### Main ###
 
-load_settings
 declare_kubeconfig
 [[ "${lazy_deploy}" = "false" ]] || deploy_env_once
 
