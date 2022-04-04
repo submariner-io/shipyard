6554e1a696f42070d80a180b4507eafe05a70d0f
Stephen Kitt <skitt@redhat.com>
2022-04-04 11:42:21 +0200

    Revert "Release multi-arch images"
    This reverts commit 354bd9633ca39f6d5f5b4f669bcca6046b6226ba.
    
    This change broke our builds, both locally and in CI; for the latter,
    see
    https://github.com/submariner-io/shipyard/runs/5812279122?check_suite_focus=true
    
    Fixes: #771
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/shared/release_images.sh
+++ scripts/shared/release_images.sh
@@ -5,14 +5,12 @@
 source ${SCRIPTS_DIR}/lib/shflags
 DEFINE_string 'tag' "${CUTTING_EDGE}" "Additional tag(s) to use for the image (prefix 'v' will be stripped)"
 DEFINE_string 'repo' 'quay.io/submariner' "Quay.io repo to deploy to"
-DEFINE_string 'oci' '' 'Directory containing OCI images (for multi-arch pushes)'
-FLAGS_HELP="USAGE: $0 [--tag v1.2.3] [--repo quay.io/myrepo] [--oci package] image [image ...]"
+FLAGS_HELP="USAGE: $0 [--tag v1.2.3] [--repo quay.io/myrepo] image [image ...]"
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
 
 release_tag="${FLAGS_tag}"
 repo="${FLAGS_repo}"
-oci="${FLAGS_oci}"
 
 if [[ $# == 0 ]]; then
     echo "At least one image to release must be specified!"
@@ -28,18 +26,13 @@ function release_image() {
 
     for target_tag in $VERSION $release_tag; do
         local target_image="${image}:${target_tag#v}"
-        if [[ -z "${oci}" ]]; then
-            # Single-arch
-            skopeo copy docker-daemon:${repo}/${image}:${DEV_VERSION} docker://${repo}/${target_image}
-        else
-            skopeo copy --all oci-archive:${oci}/${image}.tar docker://${repo}/${target_image}
-        fi
+        docker tag ${image}:${DEV_VERSION} ${target_image}
+        docker push ${target_image}
     done
 }
 
-echo "$QUAY_PASSWORD" | skopeo login quay.io -u "$QUAY_USERNAME" --password-stdin
-
-for image; do
-    release_image ${image}
+echo "$QUAY_PASSWORD" | docker login quay.io -u "$QUAY_USERNAME" --password-stdin
+for image in "$@"; do
+    release_image ${repo}/${image}
 done
 
