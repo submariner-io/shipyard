d52273820e3d04e8fbe5f92571a9acdb2c90d3bc
Mike Kolesnik <mkolesni@redhat.com>
2022-07-27 23:11:16 +0300

    Consume ENV vars for auxiliary scripts
    Instead of consuming flags, consume environment variables where
    possible.
    Some flags are retained for backwards compatibility not to break
    consumers, and will be removed once their consumers are updated.
    
    This commit handles the auxiliary scripts (compile, images, release)
    in order to have a manageable review process.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/shared/release_images.sh
+++ scripts/shared/release_images.sh
@@ -4,15 +4,10 @@
 
 source "${SCRIPTS_DIR}/lib/shflags"
 DEFINE_string 'tag' "${CUTTING_EDGE}" "Additional tag(s) to use for the image (prefix 'v' will be stripped)"
-DEFINE_string 'repo' 'quay.io/submariner' "Quay.io repo to deploy to"
-DEFINE_string 'oci' '' 'Directory containing OCI images (for multi-arch pushes)'
-FLAGS_HELP="USAGE: $0 [--tag v1.2.3] [--repo quay.io/myrepo] [--oci package] image [image ...]"
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
 
-release_tag="${FLAGS_tag}"
-repo="${FLAGS_repo}"
-oci="${FLAGS_oci}"
+[[ -n "${TAG}" ]] || TAG="${FLAGS_tag}"
 
 if [[ $# == 0 ]]; then
     echo "At least one image to release must be specified!"
@@ -24,15 +19,13 @@ set -e
 source "${SCRIPTS_DIR}/lib/debug_functions"
 
 function release_image() {
-    local image=$1
-
-    for target_tag in $VERSION $release_tag; do
+    for target_tag in $VERSION $TAG; do
         local target_image="${image}:${target_tag#v}"
-        if [[ -z "${oci}" ]]; then
+        if [[ -z "${OCIDIR}" ]]; then
             # Single-arch
-            skopeo copy "docker-daemon:${repo}/${image}:${DEV_VERSION}" "docker://${repo}/${target_image}"
+            skopeo copy "docker-daemon:${REPO}/${image}:${DEV_VERSION}" "docker://${REPO}/${target_image}"
         else
-            skopeo copy --all "oci-archive:${oci}/${image}.tar" "docker://${repo}/${target_image}"
+            skopeo copy --all "oci-archive:${OCIDIR}/${image}.tar" "docker://${REPO}/${target_image}"
         fi
     done
 }
@@ -40,6 +33,6 @@ function release_image() {
 echo "$QUAY_PASSWORD" | skopeo login quay.io -u "$QUAY_USERNAME" --password-stdin
 
 for image; do
-    release_image "${image}"
+    release_image
 done
 
