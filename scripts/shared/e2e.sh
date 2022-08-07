49bb74adad0c1074a5e8fd9e7f87ca648ad0c877
Mike Kolesnik <mkolesni@redhat.com>
2022-08-07 17:30:18 +0300

    Remove E2E_ARGS, get from settings
    We use Shipyard's settings capability everywhere, which already dictates
    the clusters deployment topology.
    Instead of forcing the users to explicitly state the clusters for E2E,
    deduce them from the settings.
    
    Special care is given to place the biggest cluster first, as some tests
    rely on that.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/shared/e2e.sh
+++ scripts/shared/e2e.sh
@@ -7,13 +7,6 @@ ginkgo_args=()
 [[ -n "${FOCUS}" ]] && ginkgo_args+=("-ginkgo.focus=${FOCUS}")
 [[ -n "${SKIP}" ]] && ginkgo_args+=("-ginkgo.skip=${SKIP}")
 
-if [[ $# == 0 ]]; then
-    echo "At least one cluster to test on must be specified!"
-    exit 1
-fi
-
-context_clusters=("$@")
-
 source "${SCRIPTS_DIR}/lib/utils"
 print_env FOCUS GLOBALNET LAZY_DEPLOY SKIP TESTDIR
 source "${SCRIPTS_DIR}/lib/debug_functions"
@@ -21,7 +14,7 @@ source "${SCRIPTS_DIR}/lib/debug_functions"
 ### Functions ###
 
 function deploy_env_once() {
-    if with_context "${context_clusters[0]}" kubectl wait --for=condition=Ready pods -l app=submariner-gateway -n "${SUBM_NS}" --timeout=3s > /dev/null 2>&1; then
+    if with_context "${clusters[0]}" kubectl wait --for=condition=Ready pods -l app=submariner-gateway -n "${SUBM_NS}" --timeout=3s > /dev/null 2>&1; then
         echo "Submariner already deployed, skipping deployment..."
         return
     fi
@@ -33,14 +26,14 @@ function deploy_env_once() {
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 function generate_kubecontexts() {
-    join_by , "${context_clusters[@]}"
+    join_by , "${clusters[@]}"
 }
 
 function test_with_e2e_tests {
     cd "${DAPPER_SOURCE}/${TESTDIR}"
 
     ${GO:-go} test -v -timeout 30m -args -ginkgo.v -ginkgo.randomizeAllSpecs -ginkgo.trace\
-        -submariner-namespace $SUBM_NS "${context_clusters[@]/#/-dp-context=}" ${gn:+"$gn"} \
+        -submariner-namespace $SUBM_NS "${clusters[@]/#/-dp-context=}" ${gn:+"$gn"} \
         -ginkgo.reportPassed -test.timeout 15m \
         "${ginkgo_args[@]}" \
         -ginkgo.reportFile "${DAPPER_OUTPUT}/e2e-junit.xml" 2>&1 | \
@@ -51,8 +44,28 @@ function test_with_subctl {
     subctl verify --only connectivity --kubecontexts "$(generate_kubecontexts)"
 }
 
+function count_nodes() {
+    wc -w <<< "${cluster_nodes[${clusters[$1]}]}"
+}
+
+# Make sure the biggest cluster is always first, as some tests rely on having a big first cluster.
+function order_clusters {
+    local biggest_cluster=0
+    for i in "${!clusters[@]}"; do
+        if [[ $(count_nodes "$i") -gt $(count_nodes "${biggest_cluster}") ]]; then
+            biggest_cluster="$i"
+        fi
+    done
+
+    local orig_cluster="${clusters[0]}"
+    clusters[0]="${clusters[$biggest_cluster]}"
+    clusters[$biggest_cluster]="${orig_cluster}"
+}
+
 ### Main ###
 
+load_settings
+order_clusters
 declare_kubeconfig
 [[ "${LAZY_DEPLOY}" != "true" ]] || deploy_env_once
 
