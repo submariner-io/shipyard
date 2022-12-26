b3a77a2eb7ae9baefe0c2f8144961ca47e02901e
Mike Kolesnik <mkolesni@redhat.com>
2022-12-26 16:25:09 +0200

    Refactor unit test script to accept `TEST_ARGS`
    Instead of passing the args on the command line, use the environment
    variable set by Make.
    
    The args don't need to be passed anymore, as the script was
    refactored to automatically ignore e2e tests.
    
    The unit test GHA has been expanded and folded into the testing of all
    scripts.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/shared/unit_test.sh
+++ scripts/shared/unit_test.sh
@@ -4,59 +4,48 @@ set -e
 
 source "${SCRIPTS_DIR}/lib/debug_functions"
 
-function _build_find_exclude() {
-    local find_exclude
-    excluded_dirs+=" vendor .git .trash-cache bin"
-
-    for dir in $excluded_dirs; do
-        find_exclude+=" -path ./$dir -prune -o"
+function _find() {
+    declare -a excludes
+    for entry in .git $(git ls-files -o -i --exclude-from=.gitignore --directory); do
+        test -f "$entry" || excludes+=(-path "./${entry/\/}" -prune -o)
     done
 
-    echo "${find_exclude}"
+    find . "${excludes[@]}" "$@" -printf "%h\0" | sort -z -u
 }
-
-function _find_pkg_dirs() {
-    # shellcheck disable=SC2046
-    find . $(_build_find_exclude) -path "$1" -printf "%h\0" | sort -z -u
-}
-
-function find_modules() {
-    # shellcheck disable=SC2046
-    find . $(_build_find_exclude) -name go.mod -printf "%h\0" | sort -z -u
-}
-
-function find_unit_test_dirs() {
-    local excluded_dirs="${*}"
-    _find_pkg_dirs "./*/*_test.go"
-}
-
-echo "Looking for packages to test"
-
-readarray -d '' modules < <(find_modules)
 
 result=0
+echo "Looking for packages to test"
+readarray -d '' modules < <(_find -name go.mod)
 
 for module in "${modules[@]}"; do
-    printf "Looking for tests in module %s\n" "${module}"
+    exclude_args=()
+    echo "Looking for tests in module ${module}"
 
-    excluded_modules=""
+    # Exclude any sub-modules
     for exc_module in "${modules[@]}"; do
         if [ "$exc_module" != "$module" ] && [ "$exc_module" != "." ]; then
-            excluded_modules+=" ${exc_module:2}"
+            exclude_args+=(-path "$exc_module" -prune -o)
         fi
     done
 
-    # Run in subshell to return to base directory in any case the subshell exits
+    # Run in subshell to return to base directory even if the tests fail
     (
         cd "$module"
-        readarray -d '' packages < <(find_unit_test_dirs "$excluded_modules" "$@")
+
+        # Exclude any directories containing e2e tests
+        for dir in $(git grep -w -l e2e | grep _test.go | sed 's#\(.*/.*\)/.*$#\1#' | sort -u); do
+            exclude_args+=(-path "./${dir}" -prune -o)
+        done
+
+        readarray -d '' packages < <(_find "${exclude_args[@]}" -path "./*/*_test.go")
         [ "${#packages[@]}" -gt 0 ] || exit 0
 
         echo "Running tests in ${packages[*]}"
         [ "${ARCH}" == "amd64" ] && race=-race
         # It's important that the `go test` command's exit status is reported from this () block.
         # Can't be one command (with -cover). Need detailed -coverprofile for Sonar and summary to console.
-        ${GO:-go} test -v ${race} -coverprofile unit.coverprofile "${packages[@]}" -ginkgo.v -ginkgo.trace -ginkgo.reportPassed -ginkgo.reportFile junit.xml "$@" && \
+        # shellcheck disable=SC2086 # Split `$TEST_ARGS` on purpose
+        ${GO:-go} test -v ${race} -coverprofile unit.coverprofile "${packages[@]}" -ginkgo.v -ginkgo.trace -ginkgo.reportPassed -ginkgo.reportFile junit.xml $TEST_ARGS && \
         go tool cover -func unit.coverprofile
     ) || result=1
 done
