47fd3c86572cd5f6e8e8cf397a04c8658f9e2f7f
Maxim Babushkin <mbabushk@redhat.com>
2023-01-04 19:37:56 +0200

    Add fips functions
    As part of FIPS test creation, add the following support functions:
    - FindFipsConfig
      The function will search for the FIPS enabled cluster.
      FIPS enablement configuration stored within "cluster-config-v1"
      configmap of "kube-system" namespace.
    
    - FindFipsEnabledCluster
      The function finds the fips enabled cluster.
      Returns clusters ID index if found.
      Otherwise, returns - "-1".
    
    - TestGatewayNodeFipsMode
      The function checks for the FIPS enabled configuration within the
      submariner gateway pod.
      Expects to find the following strings:
      - "fips mode: yes"
      - "fips mode enabled for pluto daemon"
      If not found, returns an error.
    
    Currently, FIPS is not supported on pure k8s.
    It is supported on OCP clusters.
    https://docs.openshift.com/container-platform/4.10/installing/installing-fips.html
    
    Signed-off-by: Maxim Babushkin <mbabushk@redhat.com>

---
+++ test/e2e/framework/fips.go
@@ -0,0 +1,106 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package framework
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	. "github.com/onsi/gomega"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	fipsNamespace     = "kube-system"
+	fipsConfigMapName = "cluster-config-v1"
+)
+
+func DetectFIPSConfig(cluster ClusterIndex) (bool, error) {
+	configMap, err := KubeClients[cluster].CoreV1().ConfigMaps(fipsNamespace).Get(context.TODO(), fipsConfigMapName, metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	if strings.Contains(configMap.Data["install-config"], "fips: true") {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (f *Framework) FindFIPSEnabledCluster() ClusterIndex {
+	for idx := range TestContext.ClusterIDs {
+		fipsEnabled, err := DetectFIPSConfig(ClusterIndex(idx))
+		Expect(err).NotTo(HaveOccurred())
+
+		if fipsEnabled {
+			return ClusterIndex(idx)
+		}
+	}
+
+	return -1
+}
+
+func verifyFIPSOutput(data string) bool {
+	if strings.Contains(strings.ToLower(data), "fips mode: yes") &&
+		strings.Contains(strings.ToLower(data), "fips mode enabled for pluto daemon") {
+		return true
+	}
+
+	return false
+}
+
+func (f *Framework) TestGatewayNodeFIPSMode(cluster ClusterIndex, gwPod string) {
+	By(fmt.Sprintf("Verify FIPS mode is enabled on gateway pod %q", gwPod))
+
+	ctx := context.TODO()
+	cmd := []string{"ipsec", "pluto", "--selftest"}
+
+	stdOut, stdErr, err := f.ExecWithOptions(ctx, &ExecOptions{
+		Command:       cmd,
+		Namespace:     TestContext.SubmarinerNamespace,
+		PodName:       gwPod,
+		ContainerName: SubmarinerGateway,
+		CaptureStdout: true,
+		CaptureStderr: true,
+	}, cluster)
+	Expect(err).To(Succeed())
+
+	if stdOut == "" && stdErr == "" {
+		Fail(fmt.Sprintf("No output received from command %q", cmd))
+	}
+
+	// The output of the "ipsec pluto --selftest" command could be written to stdout or stderr.
+	// Checking both outputs for the expected strings.
+	fipsStdOutResult := verifyFIPSOutput(stdOut)
+	fipsStdErrResult := verifyFIPSOutput(stdErr)
+
+	if fipsStdOutResult || fipsStdErrResult {
+		By(fmt.Sprintf("FIPS mode is enabled on gateway pod %q", gwPod))
+		return
+	}
+
+	Fail(fmt.Sprintf("FIPS mode is not enabled on gateway pod %q", gwPod))
+}
