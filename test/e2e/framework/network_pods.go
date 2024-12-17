a500c335b07eeeb68fe252c7e2d7160dd026177e
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2024-12-17 10:08:04 +0530

    Don't use -s and -p flags with ncat -l
    `-s` and `-p` flags are not supported with listen mode
    with ncat.
    
    Note: This should be handled by `nc` script but doesn't seem
    to be working. Once it works, revert this change so it works
    with older versions of nettest/ncat.
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

--- test/e2e/framework/network_pods.go
+++ test/e2e/framework/network_pods.go
@@ -278,7 +278,7 @@ func (np *NetworkPod) buildTCPCheckListenerPod() {
 						"for i in $(seq 1 $BUFS_NUM);" +
 							" do echo [dataplane] listener says $SEND_STRING;" +
 							" done" +
-							" | nc -w ${CONN_TIMEOUT}s -l -v -p $LISTEN_PORT -s 0.0.0.0 >/dev/termination-log 2>&1",
+							" | nc -w ${CONN_TIMEOUT}s -l -v 0.0.0.0 $LISTEN_PORT >/dev/termination-log 2>&1",
 					},
 					Env: []v1.EnvVar{
 						{Name: "LISTEN_PORT", Value: strconv.FormatInt(int64(np.Config.Port), 10)},
