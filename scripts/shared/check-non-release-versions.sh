68cc898c1eab53a86d7c6f900f2a79d3ea863c3e
Stephen Kitt <skitt@redhat.com>
2025-11-06 14:29:06 +0100

    Check non-release dependencies across modules
    The check for Submariner dependencies referencing unmerged commits
    only checks the main module. That isn't sufficient: Lighthouse for
    example has a coredns submodule with Submariner dependencies.
    
    This extends check-non-release-versions.sh to loop over all
    submodules. Dependencies are only cloned once, and all dependencies
    are processed before returning the result so that all invalid
    dependencies are identified.
    
    Submodules for which this check is irrelevant are processed too, but
    since they don't have Submariner dependencies that's fine; and if they
    ever do end up with Submariner dependencies, they'll be covered
    automatically.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/shared/check-non-release-versions.sh
+++ scripts/shared/check-non-release-versions.sh
@@ -8,19 +8,21 @@ trap 'rm -rf $tmpdir' EXIT
 # vX.Y.Z-0.YYYYMMDDhhmmss-hash
 failed=0
 shopt -s lastpipe
-GOWORK=off go list -m -mod=mod -json all |
-    jq -r 'select(.Path | contains("/submariner-io/")) | select(.Main != true) | select(.Version | contains ("-")) | select(.Version | length > 14) | "\(.Path) \(.Version)"' |
-    while read -r project version; do
-        cd "$tmpdir" || exit 1
-        git clone "https://$project"
-        cd "${project##*/}" || exit 1
-        hash="${version##*-}"
-        branch="${GITHUB_BASE_REF:-devel}"
-        if ! git merge-base --is-ancestor "$hash" "origin/$branch"; then
-            printf "This project depends on %s %s\n" "$project" "$version"
-            printf "but %s branch %s does not contain commit %s\n" "$project" "$branch" "$hash"
-            failed=1
-        fi
-    done
+while read -d $'\0' -r module <&3; do
+    GOWORK=off go -C "$module" list -m -mod=mod -json all |
+        jq -r 'select(.Path | contains("/submariner-io/")) | select(.Main != true) | select(.Version | contains ("-")) | select(.Version | length > 14) | "\(.Path) \(.Version)"' |
+        while read -r project version; do
+            cd "$tmpdir" || exit 1
+            [ -d "${project##*/}" ] || git clone "https://$project"
+            cd "${project##*/}" || exit 1
+            hash="${version##*-}"
+            branch="${GITHUB_BASE_REF:-devel}"
+            if ! git merge-base --is-ancestor "$hash" "origin/$branch"; then
+                printf "This project depends on %s %s\n" "$project" "$version"
+                printf "but %s branch %s does not contain commit %s\n" "$project" "$branch" "$hash"
+                failed=1
+            fi
+        done
+done 3< <(find . -name go.mod -printf '%h\0')
 
 exit $failed
