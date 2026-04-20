b306434bc296f610c8b4b5de3f7a4bd0aac36520
Stephen Kitt <skitt@redhat.com>
2026-04-20 14:52:23 +0200

    Allow UPX compression to be skipped in images
    For throw-away images, this saves significant build time whenever
    images need to be rebuilt. This is enabled for linting jobs: in most
    cases they don’t involve rebuilding images, but when they do, those
    images are rebuilt for every linting job, and are then discarded.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/shared/build_image.sh
+++ scripts/shared/build_image.sh
@@ -56,6 +56,9 @@ fi
 
 # Rebuild the image to update any changed layers and tag it back so it will be used.
 buildargs_flags=(--build-arg BUILDKIT_INLINE_CACHE=1 --build-arg "BASE_BRANCH=${BASE_BRANCH}" --build-arg "VERSION=${VERSION}")
+if [[ "${IMAGE_UPX}" = "false" ]]; then
+    buildargs_flags+=(--build-arg UPX_LEVEL=-0)
+fi
 if [[ "${PLATFORM}" != "${default_platform}" ]] && docker buildx version > /dev/null 2>&1; then
     docker buildx use buildx_builder || docker buildx create --name buildx_builder --use
     docker buildx build "${output_flag}" -t "${local_image}" "${cache_flags[@]}" -f "${dockerfile}" --iidfile "${hashfile}" --platform "${PLATFORM}" "${buildargs_flags[@]}" .
